462da868a63874a83c13cb0de9ed769c9240ea53
DieTower <rodrigonavecosta.1996@gmail.com>
2021-10-17 16:56:34 +0100

    Add jest & react-markdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,37 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 
+import { Flex, Grid, GridItem } from "@chakra-ui/layout";
+
 const Home = () => {
   
   return (
     <div>
       <h1>Hello Friend</h1>
+      <Grid
+        w="200px"
+        h="100px"
+        bgColor="red"
+        templateColumns="1fr 1fr"
+        templateRows="1fr 1fr">
+        
+        <GridItem rowSpan={2} bgColor="blue">
+          
+        </GridItem>
+        
+        <GridItem rowSpan={2} bgColor="pink">
+          <Flex
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            gridGap={4}>
+            
+            <h1>Hello1</h1>
+            <h2>Hello2</h2>
+            
+          </Flex>
+        </GridItem>
+      </Grid>
     </div>
   )
 }
